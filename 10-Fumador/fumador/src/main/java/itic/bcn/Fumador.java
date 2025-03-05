bd04cf87bd6029fb8aeda49032217aa0fd0304fc
PauSintesP <pausintespaul@gmail.com>
2025-03-05 18:20:04 +0100

    activitat acabada

--- 10-Fumador/fumador/src/main/java/itic/bcn/Fumador.java
+++ 10-Fumador/fumador/src/main/java/itic/bcn/Fumador.java
@@ -19,34 +19,13 @@ public class Fumador implements Runnable {
         System.out.println("Fumador " + id + " ha fumat " + fumades + " vegades");
     }
 
-    // Each fumador already disposes of one ingredient.
-    // They only need to purchase the two missing ingredients.
     private void compra() throws InterruptedException {
-        switch (id) {
-            // Fumador 0 already has Tabac, so buys Paper and Llumí
-            case 0:
-                System.out.println("Fumador " + id + " comprant Paper");
-                estanc.venPaper();
-                System.out.println("Fumador " + id + " comprant Llumí");
-                estanc.venLlumi();
-                break;
-            // Fumador 1 already has Paper, so buys Tabac and Llumí
-            case 1:
-                System.out.println("Fumador " + id + " comprant Tabac");
-                estanc.venTabac();
-                System.out.println("Fumador " + id + " comprant Llumí");
-                estanc.venLlumi();
-                break;
-            // Fumador 2 already has Llumí, so buys Tabac and Paper
-            case 2:
-                System.out.println("Fumador " + id + " comprant Tabac");
-                estanc.venTabac();
-                System.out.println("Fumador " + id + " comprant Paper");
-                estanc.venPaper();
-                break;
-            default:
-                throw new IllegalArgumentException("Identificador de fumador desconegut: " + id);
-        }
+        System.out.println("Fumador " + id + " comprant Tabac");
+        estanc.venTabac();
+        System.out.println("Fumador " + id + " comprant Paper");
+        estanc.venPaper();
+        System.out.println("Fumador " + id + " comprant Llumí");
+        estanc.venLlumi();
     }
 
     @Override
